40d8f3cc0abfb0ae136419ce929653f2324806c6
Simo Tumelius <simo.tumelius@gmail.com>
2018-10-12 11:15:25 +0300

    Change setup.py no longer depends on pipenv

--- setup.py
+++ setup.py
@@ -1,40 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pathlib
-import subprocess
-
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-from setuptools.command.develop import develop
 
-__requires__ = ['pipenv']
-
-packages = find_packages(exclude=['tests'])
-base_dir = pathlib.Path(__file__).parent
-
-pipenv_command = ['pipenv', 'install', ]
-pipenv_command_dev = ['pipenv', 'install', '--dev', ]
-
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-
-    def run(self):
-        subprocess.check_call(pipenv_command_dev)
-        develop.run(self)
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-
-    def run(self):
-        subprocess.check_call(pipenv_command)
-        install.run(self)
-
-
-with open(str(base_dir / 'README.md'), encoding='utf-8') as f:
+with open(str(pathlib.Path(__file__).parent / 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
 
 setup(
     name='hx711',
@@ -54,15 +24,19 @@ setup(
     },
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=packages,
+    packages=find_packages(exclude=['tests']),
     package_data={},
     include_package_data=True,
     license="MIT license",
     keywords='hx711',
-    cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
     test_suite='tests',
+    extras_require={
+            'dev': [],
+            'test': [],
+            'prod': []
+    },
+    entry_points={
+        'console_scripts': [],
+    },
 )
 
